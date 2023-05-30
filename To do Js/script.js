f1a733e24d46c8036085ff5864f0f37ae6a8a0a8
JustYahia <yahia_abo_gabal@outlook.com>
2023-05-30 16:05:35 +0300

    To Do List JS

---
+++ To do Js/script.js
@@ -0,0 +1,30 @@
+let taskNumber = 1;
+
+function add() {
+    let input = document.getElementById("task").value;
+    if (input !== "") {
+        let table = document.getElementById("mytable");
+        let row = table.insertRow(-1);
+
+        let cell1 = row.insertCell(0);
+        cell1.innerText = taskNumber;
+
+        let cell2 = row.insertCell(1);
+        cell2.innerText = input;
+
+        let cell3 = row.insertCell(2);
+        let button = document.createElement("button");
+        button.innerText = "Delete";
+        cell3.appendChild(button);
+
+
+        button.addEventListener("click", function () {
+
+            let currentRow = this.parentNode.parentNode;
+            currentRow.parentNode.removeChild(currentRow);
+        });
+
+
+        taskNumber++;
+    }
+}
