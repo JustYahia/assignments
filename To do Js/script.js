975804691b541d9ea844442f61288d2ed545a000
JustYahia <yahia_abo_gabal@outlook.com>
2023-06-05 16:25:45 +0300

    sorting list

--- To do Js/script.js
+++ To do Js/script.js
@@ -2,7 +2,8 @@ let taskNumber = 1;
 
 function add() {
     let input = document.getElementById("task").value;
-    if (input !== "") {
+    let prioirty = document.getElementById("priority").value;
+    if (input !== "" && prioirty > 0 && prioirty <= 5 && prioirty != "") {
         let table = document.getElementById("tbody");
         let row = table.insertRow(-1);
 
@@ -13,9 +14,13 @@ function add() {
         cell2.innerText = input;
 
         let cell3 = row.insertCell(2);
+        let cell4 = row.insertCell(3);
+        cell4.innerText = prioirty;
         let button = document.createElement("button");
         button.innerText = "Delete";
         cell3.appendChild(button);
+
+        let sort_btn = document.getElementById("sort_priority").value;
 
 
         button.addEventListener("click", function () {
@@ -27,7 +32,24 @@ function add() {
 
         taskNumber++;
     }
+    else if (prioirty <= 0 || prioirty > 5 || typeof prioirty == "String") {
+        alert("enter priority in range 1 to 5 ");
+    }
     else {
         alert("please enter valid data");
+    }
+}
+function sortPriority() {
+    let table = document.getElementById("tbody");
+    let rows = Array.from(table.getElementsByTagName("tr"));
+
+    rows.sort(function (row1, row2) {
+        let priority1 = parseInt(row1.cells[3].innerText);
+        let priority2 = parseInt(row2.cells[3].innerText);
+        return priority1 - priority2;
+    });
+
+    for (let i = 0; i < rows.length; i++) {
+        table.appendChild(rows[i]);
     }
 }
