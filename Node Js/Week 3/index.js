0841eee8f8e1c861ba1d75dbc3a3e3955a927c41
JustYahia <yahia_abo_gabal@outlook.com>
2023-06-21 00:03:15 +0300

    bug fixed

--- Node Js/Week 3/index.js
+++ Node Js/Week 3/index.js
@@ -12,26 +12,25 @@ app.get('/products', async (req, res) => {
   try {
     const currencyCode = req.query.CUR || 'USD';
 
-    
+   
     const response = await axios.get(`https://v6.exchangerate-api.com/v6/b40358e36ed4d64eb3a6f959/latest/USD`);
     const exchangeRate = response.data.conversion_rates[currencyCode];
 
-    // Fetch the products from the API
     const productsResponse = await axios.get(`${BASE_API_URL}/products`);
     const products = productsResponse.data;
 
    
     const categorizedProducts = {};
 
-   
+    
     products.forEach((product) => {
       const category = product.category;
 
-      
+     
       const price = product.price * exchangeRate;
 
       if (categorizedProducts.hasOwnProperty(category.name)) {
-        
+       
         categorizedProducts[category.name].products.push({
           ...product,
           price,
@@ -50,10 +49,10 @@ app.get('/products', async (req, res) => {
       }
     });
 
-    
+   
     const categorizedProductsArray = Object.values(categorizedProducts);
 
-   
+    
     res.json(categorizedProductsArray);
   } catch (error) {
     console.error('Error:', error);
@@ -63,48 +62,25 @@ app.get('/products', async (req, res) => {
 
 
 app.post('/products', async (req, res) => {
-    try {
-     
-      const { title, price, description, categoryId, images } = req.body;
-      if (!title || !price || !description || !categoryId || !images) {
-        return res.status(400).json({ error: 'Incomplete product data' });
-      }
-  
-      
-      const response = await axios.post(`${BASE_API_URL}/products`, req.body);
-  
-      
-      const {
-        title: newTitle,
-        price: newPrice,
-        description: newDescription,
-        images: newImages,
-        category,
-        id,
-        creationAt,
-        updatedAt
-      } = response.data;
-  
-
-      const transformedResponse = {
-        title: newTitle,
-        price: newPrice,
-        description: newDescription,
-        images: newImages,
-        category,
-        id,
-        creationAt,
-        updatedAt
-      };
-  
-   
-      res.json({
-        sentData: req.body,
-        responseData: transformedResponse
-      });
-    } catch (error) {
-      console.error('Error:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    
+    const { title, price, description, categoryId, images } = req.body;
+    if (!title || !price || !description || !categoryId || !images) {
+      return res.status(400).json({ error: 'Incomplete product data' });
     }
-  });
-  
+
+   
+    const response = await axios.post(`${BASE_API_URL}/products`, req.body);
+
+    
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+app.listen(5500, "127.0.0.1" , () => {
+  console.log('Server started on port 5500');
+});
